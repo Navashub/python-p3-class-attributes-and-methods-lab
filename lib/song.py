8b1150845ff70d05a868de498c486f6878ffff21
Navashub <navasmuller01@gmail.com>
2023-12-02 12:31:56 +0300

    completed assignment

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,20 @@
 class Song:
-    pass
+    count = 0
+    artists = []
+    genres = []
+    genre_count = {}
+    artist_count = {}
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+
+       
+        Song.count += 1
+
+        Song.artists.append(artist)
+        Song.artist_count = {artist: Song.artists.count(artist) for artist in Song.artists}
+
+        Song.genres.append(genre)
+        Song.genre_count = {genre: Song.genres.count(genre) for genre in Song.genres}
